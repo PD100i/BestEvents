f42b27d9dd81e9de466ef5db91d961b5218f961c
Sergey Nikonov <sn@owen.ru>
2026-05-11 15:52:46 +0300

    Добавлены интерфейсы репозиториев

---
+++ Src/BestEvents/Repositories/IBookingRepository.cs
@@ -0,0 +1,51 @@
+﻿namespace BestEvents
+{
+    /// <summary>
+    /// Интерфейс репозитория бронирований
+    /// </summary>
+    public interface IBookingRepository
+    {
+
+        /// <summary>
+        /// Создает новое бронирование  .
+        /// </summary>
+        /// <param name="booking"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<Booking> CreateBookingAsync(Booking booking, CancellationToken ct);
+
+        /// <summary>
+        /// Получение бронирования по его идентификатору. Метод возвращает результат бронирования
+        /// </summary>
+        /// <param name="bookingId"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<Booking> GetBookingAsync(Guid bookingId, CancellationToken ct);
+
+
+        /// <summary>
+        /// Получение бронирования по его идентификатору для обновления. 
+        /// Метод должен блокировать запись до завершения транзакции, чтобы избежать гонок при обработке бронирования
+        /// </summary>
+        /// <param name="bookingId"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<Booking> GetBookingForUpdateAsync(Guid bookingId, CancellationToken ct);
+
+        /// <summary>
+        /// Обновляет бронирование. Метод должен сохранять изменения в базе данных и разблокировать запись, чтобы другие транзакции могли получить доступ к ней
+        /// </summary>
+        /// <param name="booking"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<Booking> UpdateBookingAsync(Booking booking, CancellationToken ct);
+
+        /// <summary>
+        /// Возвращает список бронирований, ожидающих обработки
+        /// </summary>
+        /// <returns></returns>
+        List<Guid> GetPendingBookings();
+
+        
+    }
+}
--- Src/BestEvents/Repositories/IEventRepository.cs
+++ Src/BestEvents/Repositories/IEventRepository.cs
@@ -2,12 +2,12 @@
 namespace BestEvents
 {
     /// <summary>
-    /// Интерфейс сервиса работы с событиями
+    /// Интерфейс репозитория работы с событиями
     /// </summary>
-    public interface IEventService
+    public interface IEventRepository
     {
         /// <summary>
-        /// Возвращает все события в виде списка Event
+        /// Возвращает все события в виде списка EventDto
         /// </summary>
         /// <param name="title"></param>
         /// <param name="from"></param>
@@ -34,12 +34,19 @@ namespace BestEvents
         Task<Event> CreateEventAsync(Event _event, CancellationToken ct = default);
 
         /// <summary>
-        /// Перезаписывает событие
-        /// </summary>
-        /// <param name="_event"></param>
+        /// Получает событие по его идентификатору для последующего обновления. Метод должен блокировать запись в БД для данного события, чтобы предотвратить одновременное обновление одной и той же записи несколькими запросами. 
         /// <param name="id"></param>
         /// <param name="ct"></param>
-        Task ReplaceEventAsync(Guid id, Event _event, CancellationToken ct = default);
+        /// <returns></returns>
+        Task<Event> GetEventForUpdateAsync(Guid id, CancellationToken ct = default);
+
+        /// <summary>
+        /// Обновляет событие. Метод должен сохранять изменения в базе данных и разблокировать запись, чтобы другие транзакции могли получить доступ к ней
+        /// </summary>
+        /// <param name="_event"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<Event> UpdateEventAsync(Event _event, CancellationToken ct = default);
 
         /// <summary>
         /// Удаляет событие по его идентификатору

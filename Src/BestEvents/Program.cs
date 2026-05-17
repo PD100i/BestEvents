d31a917c34cf93579ce2b172c179c937036ffca1
= <=>
2026-05-17 17:37:23 +0300

    Добавлены миграции

--- Src/BestEvents/Program.cs
+++ Src/BestEvents/Program.cs
@@ -53,7 +53,7 @@ app.UseAuthorization();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
+    db.Database.Migrate();
 }
 
 app.MapControllers();
